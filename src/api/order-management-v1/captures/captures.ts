50796ccfd7346606a951af448d62bdd85c787ecf
Meet Dave <meet.dave@outlook.com>
2020-09-22 19:21:20 +0200

    fix: add correct capture body interface

--- src/api/order-management-v1/captures/captures.ts
+++ src/api/order-management-v1/captures/captures.ts
@@ -1,15 +1,23 @@
 import { HttpRequest, IResponse } from '../../../http-request';
-import { IOrderBody } from '../../checkout-v3';
+import { IOrderLines } from '../../checkout-v3';
 
 export interface ICaptures {
-  capture(orderId: string, body: IOrderBody): Promise<IResponse>;
+  capture(orderId: string, body: ICaptureBody): Promise<IResponse>;
+}
+
+interface ICaptureBody {
+  captured_amount: number;
+  description?: string;
+  reference?: string;
+  order_lines?: Array<IOrderLines>;
+  shipping_delay?: number;
 }
 
 export class Captures extends HttpRequest {
   /**
    * https://developers.klarna.com/api/#order-management-api-create-capture
    */
-  capture(orderId: string, body: IOrderBody): Promise<IResponse> {
+  capture(orderId: string, body: ICaptureBody): Promise<IResponse> {
     return this.invoke(
       `POST`,
       `/ordermanagement/v1/orders/${orderId}/captures`,
