113506abd06e119d3fa0cd0305c498e70aa4176a
Meet Dave <meet.dave@outlook.com>
2020-09-22 19:22:24 +0200

    feat: ordermanagement v1 -> add additional helper functions for orders
    releaseRemainingAuthorization & updateMerchangeReference

--- src/api/order-management-v1/orders/orders.ts
+++ src/api/order-management-v1/orders/orders.ts
@@ -3,6 +3,12 @@ import { HttpRequest, IResponse } from '../../../http-request';
 export interface IOrders {
   acknowledge(orderId: string): Promise<IResponse>;
   cancel(orderId: string): Promise<IResponse>;
+  releaseRemainingAuthorization(orderId: string): Promise<IResponse>;
+}
+
+interface IUpdateMerchantReferenceBody {
+  merchant_reference1: string,
+  merchant_reference2: string,
 }
 
 export class Orders extends HttpRequest {
@@ -17,6 +23,9 @@ export class Orders extends HttpRequest {
     );
   }
 
+  /**
+   * https://developers.klarna.com/api/#order-management-api-cancel-order
+  */
   cancel(orderId: string): Promise<IResponse> {
     return this.invoke(
       `POST`,
@@ -24,4 +33,27 @@ export class Orders extends HttpRequest {
       {}
     );
   }
+  
+  /**
+   * https://developers.klarna.com/api/#order-management-api-release-remaining-authorization
+  */
+  releaseRemainingAuthorization(orderId: string): Promise<IResponse> {
+    return this.invoke(
+      `POST`,
+      `/ordermanagement/v1/orders/${orderId}/release-remaining-authorization`,
+      {}
+    );
+  }
+  
+  /**
+   * https://developers.klarna.com/api/#order-management-api-update-merchant-references
+  */
+  updateMerchantReference(orderId: string, body: IUpdateMerchantReferenceBody): Promise<IResponse> {
+    return this.invoke(
+      `PATCH`,
+      `/ordermanagement/v1/orders/${orderId}/merchant-references`,
+      body
+    );
+  }
+
 }
