41c5920d85a1888b42c38683fa44597dbf41c4c6
HormCodes <horm@horm.codes>
2022-01-06 11:27:16 +0100

    refactor: isSubscription Crystallize helper

--- test/crystallize-helpers.spec.ts
+++ test/crystallize-helpers.spec.ts
@@ -1,11 +1,37 @@
-import { CrystallizeKlarnaHelpers } from '../src';
+import { CrystallizeKlarnaHelpers, CrystallizeLineItem } from '../src';
 
 describe('initiate crystallize klarna helpers library', () => {
-  it('library should have getOrderBody property', async () => {
-    const crystallizeKlarnaHelpers = new CrystallizeKlarnaHelpers({
-      host_uri: 'http://localhost:3000',
-    });
+  const crystallizeKlarnaHelpers = new CrystallizeKlarnaHelpers({
+    host_uri: 'http://localhost:3000',
+  });
 
+  it('library should have getOrderBody property', async () => {
     expect(crystallizeKlarnaHelpers).toHaveProperty('getOrderBody');
+  });
+
+  describe('isSubscription', () => {
+    it('should return true if there is subscription item', () => {
+      expect(
+        crystallizeKlarnaHelpers.isSubscription([
+          { isSubscriptionOnly: true } as CrystallizeLineItem,
+        ])
+      ).toEqual(true);
+      expect(
+        crystallizeKlarnaHelpers.isSubscription([
+          { subscriptionPlans: [{ id: '1' }] } as CrystallizeLineItem,
+        ])
+      ).toEqual(true);
+    });
+    it('should return false if there is no subscription item', () => {
+      expect(
+        crystallizeKlarnaHelpers.isSubscription([
+          { isSubscriptionOnly: false } as CrystallizeLineItem,
+        ])
+      ).toEqual(false);
+      expect(
+        crystallizeKlarnaHelpers.isSubscription([{} as CrystallizeLineItem])
+      ).toEqual(false);
+      expect(crystallizeKlarnaHelpers.isSubscription([])).toEqual(false);
+    });
   });
 });
